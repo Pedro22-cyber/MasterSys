182324342d8a4b93f959a842a32c0a976d0c832e
Pedro <shp20200@gmail.com>
2026-05-02 14:58:25 -0300

    Criação e conexão com banco de dados

---
+++ src/test/java/dev/PedroHS/mastersys/MastersysApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.PedroHS.mastersys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MastersysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
